11349645e2fc35a4b7c1bd24c0e56c63fc6c068c
Mario Merino <mario@inventures.cl>
2020-07-13 21:36:19 -0400

    logic and todo

--- src/lib/hooks/useInput.hooks.ts
+++ src/lib/hooks/useInput.hooks.ts
@@ -4,8 +4,8 @@
  * File Created: Wednesday, 8th July 2020 11:51:01 am
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Friday, 10th July 2020 11:13:12 am
- * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
+ * Last Modified: Monday, 13th July 2020 9:34:19 pm
+ * Modified By: Mario Merino (mario@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
  * Terms and conditions defined in license.txt
@@ -13,50 +13,57 @@
  * Inventures - www.inventures.cl
  */
 
-import { useState, useRef, useEffect, useCallback, useMemo } from "react";
-import { Subject, timer } from "rxjs";
-import { debounce } from "rxjs/operators";
+import { useState, useRef, useEffect, useCallback, useMemo } from 'react';
+import { Subject, timer } from 'rxjs';
+import { debounce } from 'rxjs/operators';
 
 type useInputOptions = {
-  formatter?: (input: string) => string;
-  debounceTime?: number;
-  validators?: ((input: string) => string)[];
+	formatter?: (input: string) => string;
+	debounceTime?: number;
+	validators?: ((input: string) => string)[];
 };
 export const useInput = (
-  defaultValue: string,
-  initialOptions?: useInputOptions
+	defaultValue: string,
+	initialOptions?: useInputOptions
 ): [string, (data: string) => void, boolean, string[]] => {
-  const [value, setValue] = useState<string>(defaultValue);
-  const [errors, setErrors] = useState<string[]>([]);
-  const formatSubjectRef = useRef<Subject<string>>(new Subject());
-  const validSubjectRef = useRef<Subject<string[]>>(new Subject());
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const options = useMemo(() => initialOptions, []);
-  const handleSetValue = useCallback(
-    (data: string) => {
-      setValue(data);
-      if (options && options.formatter) {
-        formatSubjectRef.current.next(options.formatter(data));
-      }
-      if (options && options.validators && options.validators.length) {
-        validSubjectRef.current.next(
-          options.validators.map((validator) => validator(data)).filter(Boolean)
-        );
-      }
-    },
-    [options]
-  );
-  useEffect(() => {
-    if (options && options.formatter) {
-      formatSubjectRef.current.subscribe((newValue) => {
-        setValue(newValue);
-      });
-    }
-    if (options && options.validators && options.validators.length) {
-      validSubjectRef.current
-        .pipe(debounce(() => timer(options.debounceTime || 1000)))
-        .subscribe((newErrors) => setErrors(newErrors));
-    }
-  }, [options]);
-  return [value, handleSetValue, errors.length === 0, errors];
+	const [value, setValue] = useState<string>(defaultValue);
+	const [errors, setErrors] = useState<string[]>([]);
+	const formatSubjectRef = useRef<Subject<string>>(new Subject());
+	const validSubjectRef = useRef<Subject<string[]>>(new Subject());
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const options = useMemo(() => initialOptions, []);
+	const handleSetValue = useCallback(
+		(data: string) => {
+			// todo add mask character limits - as optional parameter (applies for phone numbers, rut, not for name, email)
+			let shortData = data.slice(0, 12);
+			setValue(shortData);
+			if (options && options.formatter) {
+				formatSubjectRef.current.next(options.formatter(shortData));
+			}
+			if (options && options.validators && options.validators.length) {
+				// todo add regex to validate min and max length and/or syntax
+				validSubjectRef.current.next(
+					options.validators
+						.map((validator) => validator(shortData))
+						.filter(Boolean)
+				);
+			}
+		},
+		[options]
+	);
+	useEffect(() => {
+		if (options && options.formatter) {
+			formatSubjectRef.current.subscribe((newValue) => {
+				setValue(newValue);
+				// clean errors if user resume typing
+				setErrors([]);
+			});
+		}
+		if (options && options.validators && options.validators.length) {
+			validSubjectRef.current
+				.pipe(debounce(() => timer(options.debounceTime || 1000)))
+				.subscribe((newErrors) => setErrors(newErrors));
+		}
+	}, [options]);
+	return [value, handleSetValue, errors.length === 0, errors];
 };
