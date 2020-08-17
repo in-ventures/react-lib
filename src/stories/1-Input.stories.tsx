da9aeabb05d8ae6fd327c108b8ede446226ce16c
pita-horn <ehorn@uc.cl>
2020-08-17 18:09:54 -0400

    feat: created phone input and numeric validator

--- src/stories/1-Input.stories.tsx
+++ src/stories/1-Input.stories.tsx
@@ -4,18 +4,24 @@
  * File Created: Wednesday, 8th July 2020 1:55:18 am
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Thursday, 13th August 2020 11:19:25 am
- * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
+ * Last Modified: Monday, 17th August 2020 6:00:10 pm
+ * Modified By: Esperanza Horn (esperanza@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
  * Terms and conditions defined in license.txt
  * -----
  * Inventures - www.inventures.cl
  */
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, ChangeEvent } from 'react';
 import { number, text } from '@storybook/addon-knobs';
 import { Input } from '../components/input';
+
 import { InputStatus, useInput } from '../hooks/useInput.hooks';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+import MenuItem from '@material-ui/core/MenuItem';
 import { rutFormat } from 'rut-helpers';
 import {
   Validator,
@@ -23,7 +29,20 @@ import {
   RutFormatValidator,
   EmailValidator,
   RutValidator,
+  PhoneValidator,
+  NumericValidator,
 } from '../hooks/validators';
+import { LatinEmailFormatter } from '../hooks/formatters';
+
+type InputforPhoneProps = {
+  possibleCountries: [
+    {
+      countryName: string;
+      countryDigitLength: Number;
+      countryPrefix: Number;
+    }
+  ];
+}
 
 export default {
   title: 'Input',
@@ -94,6 +113,7 @@ export const InputForEmail = () => {
   );
   const debounceTime = number('Debounce time (ms)', 800);
   const [value, setValue, status, errors, handleBlur] = useInput('', {
+    formatter: LatinEmailFormatter,
     validators: [
       required && new RequiredValidator(required),
       incomplete && new EmailValidator(incomplete),
@@ -120,5 +140,109 @@ export const InputForEmail = () => {
     />
   );
 };
+
+export const InputForPhone = (props: InputforPhoneProps) => {
+  const classes = useStyles();
+
+  //const { possibleCountries } = props;
+
+  //Test purpose only:
+  const possibleCountries = [
+    {
+      countryName: 'Chile',
+      countryDigitLength: 9,
+      countryPrefix: 56,
+    },
+    {
+      countryName: 'Peru',
+      countryDigitLength: 9,
+      countryPrefix: 51,
+    },
+    {
+      countryName: 'USA',
+      countryDigitLength: 11,
+      countryPrefix: 1,
+    },
+  ];
+
+
+  const nonNumeric = text('Teléfono no numérico error','¡Ups! Recuerda incluir sólo números');
+  const required = text('Teléfono requerido error', 'Teléfono Requerido');
+  const incompleteNumber = text(
+    'Teléfono incompleto error',
+    '¡Ojo! Debes incluir la cantidad correcta de digitos para tu país',
+  );
+  const debounceTime = number('Debounce time (ms)', 800);
+  
+  const [countryDigits, setCountryDigits] = useState(possibleCountries[0].countryDigitLength);
+  const [country, setCountry] = useState(possibleCountries[0].countryName);
+  
+  const handleChange = (event: ChangeEvent<{ value: unknown}>) => {
+    setCountry(event.target.value as string);
+    const newDigits = possibleCountries.find(x => x.countryName === event.target.value as string).countryDigitLength;
+    if (newDigits) setCountryDigits(newDigits);
+    
+  };
+  
+  const [value, setValue, status, errors, handleBlur] = useInput('', {
+    //formatter: rutFormat,
+    validators: [
+      required && new RequiredValidator(required),
+      nonNumeric && new NumericValidator(nonNumeric),
+      incompleteNumber && new PhoneValidator(incompleteNumber, countryDigits),
+    ].filter(Boolean) as Validator<string>[],
+    debounceTime,
+  });
+  
+  const handleWrite = useCallback(
+    (e) => {
+      setValue(String(e.target.value));
+    },
+    [setValue],
+  );
+
+  console.log('status: ', status);
+  console.log('erros: ', errors);
+  //console.log('validator digits', variableValidator.countryDigits);
+
+
+  return (
+    <>
+    <FormControl className={classes.formControl}>
+      <Select
+        value={country}
+        onChange={handleChange}
+      >
+        {possibleCountries.map((item, index) => (
+        <MenuItem
+          key={index}
+          value={item.countryName}
+        >
+          {item.countryName} (+{item.countryPrefix})
+        </MenuItem>
+        ))}
+      </Select>
+      <Input
+        value={value}
+        onChange={handleWrite}
+        onBlur={handleBlur}
+        error={status === InputStatus.ERROR}
+        helperText={errors[0]}
+        label={`Phone* (${debounceTime}ms)`}
+      />
+    </FormControl>
+  </>
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 100,
+      flexDirection: 'row',
+    },
+  }),
+);
 
 Base.storyName = 'Base element';
