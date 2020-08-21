e5a867d4ffa0f6a6cba74eb6af9cad7ad18d7855
pita-horn <ehorn@uc.cl>
2020-08-21 09:48:17 -0400

    fix: fixed PhoneValidator error, where error msg was shown before checking if an error was present

--- src/hooks/validators/PhoneValidator.ts
+++ src/hooks/validators/PhoneValidator.ts
@@ -4,7 +4,7 @@
  * File Created: Friday, 14th August 2020 3:09:48 pm
  * Author: Esperanza Horn (esperanza@inventures.cl)
  * -----
- * Last Modified: Friday, 21st August 2020 9:17:55 am
+ * Last Modified: Friday, 21st August 2020 9:43:25 am
  * Modified By: Esperanza Horn (esperanza@inventures.cl)
  * -----
  * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -30,9 +30,10 @@ export class LengthValidator extends Validator {
 
   validate(input: string, maxLength: number) {
     if (maxLength) {
+      if (input.length != maxLength)
       this.errorMsg =
         '¡Ojo! Te faltan ' + (maxLength - input.length) + ' dígitos';
-      if (input.length > maxLength)
+      if (input.length != maxLength && input.length > maxLength)
         this.errorMsg =
           '¡Ojo! Te has pasado por ' + (input.length - maxLength) + ' dígitos';
       return Boolean(input.length == maxLength);
