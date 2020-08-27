ed081ea449095691794fe18a85fc0926ea325d10
Gabriel <gabriel.ulloa.e@gmail.com>
2020-08-27 18:35:20 -0400

    feat: example to handle rerender

--- src/hooks/useExample.hooks.test.ts
+++ src/hooks/useExample.hooks.test.ts
@@ -4,7 +4,7 @@
  * File Created: Thursday, 13th August 2020 11:43:43 am
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Thursday, 13th August 2020 11:52:41 am
+ * Last Modified: Thursday, 27th August 2020 6:31:57 pm
  * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -13,7 +13,7 @@
  * Inventures - www.inventures.cl
  */
 
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { renderHook, act } from '@testing-library/react-hooks';
 function useCounter(initialValue = 0) {
   const [count, setCount] = useState(initialValue);
@@ -24,6 +24,7 @@ function useCounter(initialValue = 0) {
   );
   return { count, increment, incrementAsync };
 }
+
 describe('sould use counter', () => {
   test('no initial value', () => {
     const { result } = renderHook(() => useCounter());
@@ -67,5 +68,26 @@ describe('sould increment counter', () => {
 
     await waitForNextUpdate();
     expect(result.current.count).toBe(1);
+  });
+});
+
+function useRerenderHook(value = 0) {
+  const [count, setCount] = useState(value);
+  useEffect(() => {
+    setCount(value);
+  }, [value]);
+  return count;
+}
+describe('try rerender', () => {
+  test('force', () => {
+    const { result, rerender } = renderHook(
+      (prop) => useRerenderHook(prop.count),
+      { initialProps: { count: 0 } },
+    );
+
+    expect(result.current).toBe(0);
+
+    rerender({ count: 5 });
+    expect(result.current).toBe(5);
   });
 });
