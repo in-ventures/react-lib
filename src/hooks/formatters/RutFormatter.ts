fe044acd87c44cebbf10c2c2df3275a28a34bf6e
Mario Merino <mario@inventures.cl>
2020-08-28 14:18:25 -0400

    feat: limit max length in rut formatter (#47)

--- src/hooks/formatters/RutFormatter.ts
+++ src/hooks/formatters/RutFormatter.ts
@@ -4,8 +4,8 @@
  * File Created: Tuesday, 25th August 2020 4:00:26 pm
  * Author: Esperanza Horn (esperanza@inventures.cl)
  * -----
- * Last Modified: Tuesday, 25th August 2020 4:01:34 pm
- * Modified By: Esperanza Horn (esperanza@inventures.cl)
+ * Last Modified: Friday, 28th August 2020 11:05:27 am
+ * Modified By: Mario Merino (mario@inventures.cl)
  * -----
  * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
  * Terms and conditions defined in license.txt
@@ -17,12 +17,11 @@ import { rutFormat } from 'rut-helpers';
 import { Formatter } from './Formatter';
 
 export class RutFormatter extends Formatter {
-    format(input: string) {
-      const formattedText = rutFormat(input);
-      return formattedText;
-    }
+  format(input: string) {
+    // limit the max amount of characters
+    const inputSliced = input.slice(0, 12);
+    // format and validate
+    const formattedText = rutFormat(inputSliced);
+    return formattedText;
   }
-
-
-
-
+}
