d62df8ee546f5fc6b7e88ede3a5e8c79153cde3d
Gabriel <gabriel.ulloa.e@gmail.com>
2020-09-01 17:21:44 -0400

    feat: add support for specific chars

--- src/formatters/MaskFormatter.test.ts
+++ src/formatters/MaskFormatter.test.ts
@@ -4,7 +4,7 @@
  * File Created: Tuesday, 1st September 2020 4:52:23 pm
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Tuesday, 1st September 2020 4:56:36 pm
+ * Last Modified: Tuesday, 1st September 2020 5:07:45 pm
  * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -35,5 +35,20 @@ describe('Mask formatter', () => {
     expect(formatter.format('123.')).toBe('123.');
     expect(formatter.format('123.45')).toBe('123...45');
     expect(formatter.format('1234.5')).toBe('123...4.5');
+  });
+  test('Format with alphabetic mask', () => {
+    const formatter = new MaskFormatter('AA');
+    expect(formatter.format('a1')).toBe('a');
+    expect(formatter.format('ab1')).toBe('ab');
+    expect(formatter.format('1a')).toBe('a');
+    expect(formatter.format('12a')).toBe('a');
+  });
+
+  test('Format with numeric mask', () => {
+    const formatter = new MaskFormatter('99');
+    expect(formatter.format('a1')).toBe('1');
+    expect(formatter.format('ab1')).toBe('1');
+    expect(formatter.format('1a')).toBe('1');
+    expect(formatter.format('12a')).toBe('12');
   });
 });
--- src/formatters/MaskFormatter.ts
+++ src/formatters/MaskFormatter.ts
@@ -4,7 +4,7 @@
  * File Created: Tuesday, 1st September 2020 3:45:13 pm
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Tuesday, 1st September 2020 5:01:05 pm
+ * Last Modified: Tuesday, 1st September 2020 5:21:31 pm
  * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -15,6 +15,8 @@
 
 import { Formatter } from './Formatter';
 
+const SUPPORTED_MASK_CHAR_REPLACE = ['*', 'A', '9'];
+
 export class MaskFormatter extends Formatter {
   mask: string;
   constructor(mask: string) {
@@ -24,8 +26,9 @@ export class MaskFormatter extends Formatter {
   /**
    * Format the string following a mask
    * MaskChar replace supported:
-   * - *
-   *
+   * - *: replace any char
+   * - A: replace a alphabetic char
+   * - 9: replace a numeric char
    * Replace all mask char supported with the input char given.
    * If the mask have chars not supported for replace, the char will be put in the output string
    * For examples, check the test file
@@ -37,14 +40,27 @@ export class MaskFormatter extends Formatter {
     input.split('').forEach((char) => {
       if (missingMask.length === 0) return;
       let maskChar = missingMask[0];
+      // If current mask char is supported, but current char doesn't match, omit char
+      if (
+        SUPPORTED_MASK_CHAR_REPLACE.includes(maskChar) &&
+        ((maskChar === 'A' && !char.match(/[a-zA-Z]/)) ||
+          (maskChar === '9' && !char.match(/[0-9]/)))
+      )
+        return;
       missingMask = missingMask.slice(1);
       if (maskChar === char) return (newText += char);
-      while (missingMask.length > 0 && maskChar !== '*') {
+
+      while (
+        missingMask.length > 0 &&
+        !SUPPORTED_MASK_CHAR_REPLACE.includes(maskChar)
+      ) {
         newText += maskChar;
         maskChar = missingMask[0];
         missingMask = missingMask.slice(1);
       }
       if (maskChar === '*') return (newText += char);
+      if (maskChar === 'A' && char.match(/[a-zA-Z]/)) return (newText += char);
+      if (maskChar === '9' && char.match(/[0-9]/)) return (newText += char);
     });
     const index = newText.indexOf('*');
     return index > -1 ? newText.slice(0, index) : newText;
