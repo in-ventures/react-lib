e525af0f37a7d6d2a900922aa3dac64499a23865
pita-horn <ehorn@uc.cl>
2020-09-03 18:20:32 -0400

    fix: fixed font weights

--- src/config/themes/meki.ts
+++ src/config/themes/meki.ts
@@ -4,7 +4,7 @@
  * File Created: Thursday, 9th July 2020 8:05:19 am
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Wednesday, 2nd September 2020 6:28:53 pm
+ * Last Modified: Thursday, 3rd September 2020 6:18:24 pm
  * Modified By: Esperanza Horn (esperanza@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -33,79 +33,79 @@ export const mekiTheme = themeGenerator({
     h1: {
       fontSize: '93px',
       lineHeight: '131px',
-      fontWeight: 'lighter',
+      fontWeight: 300,
       letterSpacing: '-1.5px',
     },
     h2: {
       fontSize: '58px',
       lineHeight: '81px',
-      fontWeight: 'lighter',
+      fontWeight: 400,
       letterSpacing: '-0.5px',
     },
     h3: {
       fontSize: '46px',
       lineHeight: '64px',
-      fontWeight: 'normal',
+      fontWeight: 400,
       letterSpacing: '0px',
     },
     h4: {
       fontSize: '33px',
       lineHeight: '47px',
-      fontWeight: 'normal',
+      fontWeight: 400,
       letterSpacing: '0.25px',
     },
     h5: {
       fontSize: '23px',
       lineHeight: '32px',
-      fontWeight: 'normal',
+      fontWeight: 400,
       letterSpacing: '0px',
     },
     h6: {
       fontSize: '19px',
       lineHeight: '27px',
-      fontWeight: 'bold',
+      fontWeight: 500,
       letterSpacing: '0.15px',
     },
     subtitle1: {
       fontSize: '15px',
       lineHeight: '21px',
-      fontWeight: 'normal',
+      fontWeight: 400,
       letterSpacing: '0.15px',
     },
     subtitle2: {
       fontSize: '13px',
       lineHeight: '19px',
-      fontWeight: 'bold',
+      fontWeight: 500,
       letterSpacing: '0.1px',
     },
     body1: {
       fontSize: '15px',
       lineHeight: '21px',
-      fontWeight: 'normal',
+      fontWeight: 400,
       letterSpacing: '0.15px',
     },
     body2: {
       fontSize: '13px',
       lineHeight: '19px',
-      fontWeight: 'normal',
+      fontWeight: 400,
       letterSpacing: '0.15px',
     },
     caption: {
       fontSize: '12px',
       lineHeight: '17px',
-      fontWeight: 'normal',
+      fontWeight: 400,
       letterSpacing: '0.4px',
     },
     overline: {
       fontSize: '10px',
       lineHeight: '15px',
-      fontWeight: 'normal',
+      fontWeight: 400,
       letterSpacing: '1.5px',
     },
     button: {
       fontSize: '13px',
       lineHeight: '19px',
-      fontWeight: 'normal',
+      fontWeight: 500,
       letterSpacing: '1.25px',
     },
   },
