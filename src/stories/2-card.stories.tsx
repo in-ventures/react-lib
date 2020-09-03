0bd00d752bc45f6cfd41006566c8c7a998be5ee6
pita-horn <ehorn@uc.cl>
2020-09-03 17:27:58 -0400

    fix: fixed minor bugs and linter errors

--- src/stories/2-card.stories.tsx
+++ src/stories/2-card.stories.tsx
@@ -4,7 +4,7 @@
  * File Created: Tuesday, 4th August 2020 5:47:50 pm
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Thursday, 3rd September 2020 5:08:41 pm
+ * Last Modified: Thursday, 3rd September 2020 5:21:16 pm
  * Modified By: Esperanza Horn (esperanza@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -31,7 +31,7 @@ export const Base = () => (
   />
 );
 
-export const EditableCardB = () => {
+export const EditableCard = () => {
   return (
     <ProductCard
       imageUrl={
@@ -77,7 +77,6 @@ export const ProductCarousel = () => {
           details: '30 comprimidos recubiertos',
           description: '500 mg',
           price: 15990,
-          //tag: 'Receta retenida',
           onClickCard: () => console.log('You clicked B2!'),
         },
         {
