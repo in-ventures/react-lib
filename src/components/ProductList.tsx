577b3d99b4744d9681f4349ab9561ccb3a88d28f
pita-horn <ehorn@uc.cl>
2020-09-11 11:10:07 -0300

    refactor: now each component is on a specific file

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,130 @@
+/*
+ * File: ProductList.tsx
+ * Project: @inventures/react-lib
+ * File Created: Friday, 11th September 2020 10:18:24 am
+ * Author: Esperanza Horn (esperanza@inventures.cl)
+ * -----
+ * Last Modified: Friday, 11th September 2020 10:51:13 am
+ * Modified By: Esperanza Horn (esperanza@inventures.cl)
+ * -----
+ * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
+ * Terms and conditions defined in license.txt
+ * -----
+ * Inventures - www.inventures.cl
+ */
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { ProductPropTypes, ProductCard } from './ProductCard';
+
+type ProductList = {
+  products: ProductPropTypes[];
+  gridBreakpoints: GridBreakpoints;
+};
+type GridBreakpoints = {
+  xs:
+    | boolean
+    | 'auto'
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | undefined;
+  sm:
+    | boolean
+    | 'auto'
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | undefined;
+  md:
+    | boolean
+    | 'auto'
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | undefined;
+  lg:
+    | boolean
+    | 'auto'
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | undefined;
+  xl:
+    | boolean
+    | 'auto'
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | undefined;
+};
+
+export function ProductList(props: ProductList) {
+  const {
+    products,
+    gridBreakpoints: { xs = 6, sm = 4, md = 3, lg = 2, xl = 1 } = {},
+  } = props;
+  return (
+    <Grid container spacing={1}>
+      {products.map((cardInfo: ProductPropTypes, index: number) => (
+        <Grid key={index} item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+          <ProductCard
+            imageUrl={cardInfo.imageUrl}
+            title={cardInfo.title}
+            subtitle={cardInfo.subtitle}
+            details={cardInfo.details}
+            description={cardInfo.description}
+            price={cardInfo.price}
+            tagText={cardInfo.tagText}
+            tagIcon={cardInfo.tagIcon}
+            onClickCard={cardInfo.onClickCard}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
---
+++ src/components/ProductListHeader.tsx
@@ -0,0 +1,64 @@
+/*
+ * File: ProductListHeader.tsx
+ * Project: @inventures/react-lib
+ * File Created: Friday, 11th September 2020 10:18:40 am
+ * Author: Esperanza Horn (esperanza@inventures.cl)
+ * -----
+ * Last Modified: Friday, 11th September 2020 10:52:17 am
+ * Modified By: Esperanza Horn (esperanza@inventures.cl)
+ * -----
+ * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
+ * Terms and conditions defined in license.txt
+ * -----
+ * Inventures - www.inventures.cl
+ */
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ChevronRightRoundedIcon from '@material-ui/icons/ChevronRightRounded';
+import { IconButton, Typography, Box } from '@material-ui/core';
+
+type ProductListHeader = {
+  title?: string;
+  onClickCarousel?: () => void;
+};
+
+const useStyles = makeStyles({
+  carouselHeader: {
+    paddingBottom: '8px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+  },
+  viewMore: {
+    padding: '0px',
+  },
+});
+
+export function ProductListHeader(props: ProductListHeader) {
+  const { title, onClickCarousel } = props;
+  const classes = useStyles();
+
+  return (
+    <Box>
+      {title && (
+        <Box className={classes.carouselHeader}>
+          <Typography variant="h6" color="textPrimary">
+            {title}
+          </Typography>
+
+          {onClickCarousel && (
+            <IconButton
+              color="primary"
+              aria-label="ver más resultados"
+              onClick={onClickCarousel}
+              className={classes.viewMore}
+            >
+              <ChevronRightRoundedIcon fontSize="large" />
+            </IconButton>
+          )}
+        </Box>
+      )}
+    </Box>
+  );
+}
