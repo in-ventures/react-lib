ac1056735ca5ba880418e1a20ca404f49857e34a
Gabriel <gabriel.ulloa.e@gmail.com>
2020-09-22 17:38:34 -0300

    core: Version 0.2.0

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,8 +4,8 @@
  * File Created: Friday, 31st July 2020 3:19:47 pm
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Tuesday, 1st September 2020 2:26:54 pm
- * Modified By: Luis Aparicio (luis@inventures.cl)
+ * Last Modified: Monday, 21st September 2020 6:25:37 pm
+ * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
  * Terms and conditions defined in license.txt
@@ -14,3 +14,4 @@
  */
 export * from './useInput.hooks';
 export * from './useSearchBar.hooks';
+export * from './toast.hook';
