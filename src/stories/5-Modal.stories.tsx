0c0e6ddf5ca8afac56b25b3b98a0e4202782859a
pita-horn <ehorn@uc.cl>
2020-09-23 17:05:00 -0300

    feat: added modal story using knobs

---
+++ src/stories/5-Modal.stories.tsx
@@ -0,0 +1,51 @@
+/*
+ * File: 5-Modal.stories.tsx
+ * Project: @inventures/react-lib
+ * File Created: Wednesday, 23rd September 2020 4:11:55 pm
+ * Author: Esperanza Horn (esperanza@inventures.cl)
+ * -----
+ * Last Modified: Wednesday, 23rd September 2020 5:03:08 pm
+ * Modified By: Esperanza Horn (esperanza@inventures.cl)
+ * -----
+ * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
+ * Terms and conditions defined in license.txt
+ * -----
+ * Inventures - www.inventures.cl
+ */
+
+import React, {useState} from 'react';
+import {
+  AlertModal
+} from '../components';
+import Button from '@material-ui/core/Button';
+import { text, number } from '@storybook/addon-knobs';
+
+export default {
+  title: 'Modal',
+};
+
+export const CustomModal = () => {
+    const [open, setOpen] = useState(false);
+    const titleText = text('titleText', 'Validación receta');
+    const contentText = text('contentText', '¡Gracias! Nuestros químicos farmacéuticos van a verificar que todo esté en orden 👨🏻‍⚕️👩🏻‍⚕️ Te avisaremos si hay algún problema');
+
+    return (
+        <>
+            <Button variant="outlined" color="primary" onClick={() => setOpen(true)}>
+                Open alert dialog
+            </Button>
+            <AlertModal
+                title={titleText}
+                content={contentText}
+                open={open}
+                setOpen={setOpen}
+                actions={[
+                    {
+                        text: 'OK',
+                        onActionClick: () => setOpen(false),
+                    }
+                ]}
+            />
+        </>
+    );
+};
