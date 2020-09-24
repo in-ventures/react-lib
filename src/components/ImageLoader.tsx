7dcdeebe945a69bd9f0f3e88e00914572aa32cc8
Gabriel Ulloa <gabriel@inventures.cl>
2020-09-24 16:12:49 -0300

    fix: build storybook error (#111)
    * fix: build storybook error
    
    * feat: GH to verify storybook build

--- src/components/ImageLoader.tsx
+++ src/components/ImageLoader.tsx
@@ -3,13 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import { TextFieldProps } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import Divider from '@material-ui/core/Divider';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import CardContent from '@material-ui/core/CardContent';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles({
--- src/validators/DateValidator.ts
+++ src/validators/DateValidator.ts
@@ -4,8 +4,8 @@
  * File Created: Thursday, 3rd September 2020 6:40:13 pm
  * Author: Esperanza Horn (esperanza@inventures.cl)
  * -----
- * Last Modified: Friday, 4th September 2020 4:40:57 pm
- * Modified By: Esperanza Horn (esperanza@inventures.cl)
+ * Last Modified: Thursday, 24th September 2020 3:12:13 pm
+ * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
  * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
  * Terms and conditions defined in license.txt
@@ -18,12 +18,10 @@ export class DateValidator extends Validator {
   dateRegex = new RegExp(/^([\d+]{2})\/([\d+]{2})\/([\d+]{4})$/);
   constructor(errorMsg: string) {
     super(errorMsg);
-    this.dateRegex = this.dateRegex;
   }
   validate(input: string) {
     if (Boolean(input.match(this.dateRegex))) {
       const date = input.split('/');
-      const inputDate = new Date(+date[2], +date[1] - 1, +date[0]);
       const correctDayMonth =
         +date[0] >= 1 && +date[0] <= 31 && +date[1] >= 1 && +date[1] <= 12;
       const thisYear = new Date().getFullYear();
