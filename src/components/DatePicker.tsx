3d2eb1421e0b9f128dd65e384967415cc230295f
pita-horn <ehorn@uc.cl>
2020-09-25 16:08:13 -0300

    created new Calendar component

---
+++ src/components/DatePicker.tsx
@@ -0,0 +1,51 @@
+/*
+ * File: DatePicker.tsx
+ * Project: @inventures/react-lib
+ * File Created: Friday, 25th September 2020 3:06:54 pm
+ * Author: Esperanza Horn (esperanza@inventures.cl)
+ * -----
+ * Last Modified: Friday, 25th September 2020 4:06:38 pm
+ * Modified By: Esperanza Horn (esperanza@inventures.cl)
+ * -----
+ * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
+ * Terms and conditions defined in license.txt
+ * -----
+ * Inventures - www.inventures.cl
+ */
+
+import 'date-fns';
+import React from 'react';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+type CustomCalendarProps = {
+    variant ?: "dialog" | "inline" | "static" | undefined;
+    selectedDate: Date | null;
+    setSelectedDate: (date: Date | null) => void;
+};
+
+export function Calendar(props: CustomCalendarProps) {
+  const { variant, selectedDate, setSelectedDate } = props;
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          disableToolbar
+          variant={variant}
+          format="dd/MM/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={selectedDate}
+          onChange={handleDateChange}
+        />
+    </MuiPickersUtilsProvider>
+  );
+}
