e80e152b26389c9ab32ac40db92236f39c79b2c3
Gabriel Ulloa <gabriel@inventures.cl>
2020-10-14 15:21:09 -0300

    bugfix: useDebounce (#152)
    * bugfix: useDebounce
    
    * fix: clear dependencies
    
    * fix: useSearchBar hook dependencies

--- src/hooks/useDebouncedCallback.ts
+++ src/hooks/useDebouncedCallback.ts
@@ -4,7 +4,7 @@
  * File Created: Wednesday, 7th October 2020 11:59:01 am
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Friday, 9th October 2020 5:07:03 pm
+ * Last Modified: Wednesday, 14th October 2020 1:20:41 pm
  * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -13,19 +13,22 @@
  * Inventures - www.inventures.cl
  */
 
-import { useCallback, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import debounce from 'lodash/debounce';
 
 export function useDebouncedCallback(
   callback: (...args: any[]) => any,
   debounceTime: number,
-  dependencies: any[],
 ) {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const executableCallback = useCallback(callback, dependencies);
+  const executableCallback = useRef(callback);
+  useEffect(() => {
+    console.log('change');
+    executableCallback.current = callback;
+  }, [callback]);
   const debounceRef = useRef(
     debounce((...args) => {
-      executableCallback(...args);
+      console.log('execute debounce');
+      executableCallback.current(...args);
     }, debounceTime),
   );
   return debounceRef.current;
--- src/stories/0-hooks.stories.tsx
+++ src/stories/0-hooks.stories.tsx
@@ -4,7 +4,7 @@
  * File Created: Friday, 9th October 2020 4:50:42 pm
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Friday, 9th October 2020 5:03:28 pm
+ * Last Modified: Wednesday, 14th October 2020 1:22:14 pm
  * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
  * Copyright 2019 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -14,8 +14,6 @@
  */
 
 import React, { useState } from 'react';
-
-import { text, number } from '@storybook/addon-knobs';
 import { useDebouncedCallback } from '../hooks';
 
 export default {
@@ -24,13 +22,10 @@ export default {
 export const useDebouncedCallbackExample = () => {
   const [count, setCount] = useState(0);
   const [debouncedCount, setDebouncedCount] = useState(0);
-  const debouncedIncrement = useDebouncedCallback(
-    () => {
-      setDebouncedCount(count);
-    },
-    600,
-    [setDebouncedCount, count],
-  );
+  const debouncedIncrement = useDebouncedCallback(() => {
+    console.log('debounced count', { count });
+    setDebouncedCount(count);
+  }, 600);
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <button
