1f114609bbd10bdaa828d219e0728e9a1f9caa2b
VicenteMelin <37168631+VicenteMelin@users.noreply.github.com>
2021-02-01 11:38:08 -0300

    fix: clp currency formatter fix (#280)
    * fix: clp currency formatter fix
    
    * fix: force string input

--- src/formatters/CurrencyFormatter.ts
+++ src/formatters/CurrencyFormatter.ts
@@ -4,8 +4,8 @@
  * File Created: Monday, 31st August 2020 6:08:15 pm
  * Author: Esperanza Horn (esperanza@inventures.cl)
  * -----
- * Last Modified: Wednesday, 4th November 2020 1:00:07 pm
- * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
+ * Last Modified: Monday, 1st February 2021 11:30:29 am
+ * Modified By: Vicente Melin (vicente@inventures.cl)
  * -----
  * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
  * Terms and conditions defined in license.txt
@@ -17,13 +17,8 @@ import { Formatter } from './Formatter';
 
 export class CurrencyFormatter extends Formatter {
   format(input: number) {
-    const formatter = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
-    });
-    const formattedText = formatter.format(input);
-    return formattedText.replace(/,/g, '.');
+    return (
+      '$ ' + String(input.toFixed(0)).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') //separate every 3 digits and use . to separate
+    );
   }
 }
