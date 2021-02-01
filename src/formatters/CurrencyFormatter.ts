4c0cdbc8d7d85e7b0084ef5381d04f9e5a0e05a2
Vicente <vtmelin@uc.cl>
2021-02-01 11:31:33 -0300

    fix: force string input

--- src/formatters/CurrencyFormatter.ts
+++ src/formatters/CurrencyFormatter.ts
@@ -4,7 +4,7 @@
  * File Created: Monday, 31st August 2020 6:08:15 pm
  * Author: Esperanza Horn (esperanza@inventures.cl)
  * -----
- * Last Modified: Monday, 1st February 2021 11:21:15 am
+ * Last Modified: Monday, 1st February 2021 11:30:29 am
  * Modified By: Vicente Melin (vicente@inventures.cl)
  * -----
  * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -18,10 +18,7 @@ import { Formatter } from './Formatter';
 export class CurrencyFormatter extends Formatter {
   format(input: number) {
     return (
-      '$ ' +
-      input
-        .toFixed(0) // always zero decimal digits
-        .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') //separate every 3 digits and use . to separate
+      '$ ' + String(input.toFixed(0)).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') //separate every 3 digits and use . to separate
     );
   }
 }
