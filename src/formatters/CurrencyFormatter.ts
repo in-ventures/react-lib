bf8a6a51ffaca80765582b6ff93b5960baebff20
Gabriel Ulloa <gabriel.ulloa.e@gmail.com>
2021-05-17 20:17:41 -0400

    fix: handle currency formatter errors

--- src/formatters/CurrencyFormatter.ts
+++ src/formatters/CurrencyFormatter.ts
@@ -4,8 +4,8 @@
  * File Created: Monday, 31st August 2020 6:08:15 pm
  * Author: Esperanza Horn (esperanza@inventures.cl)
  * -----
- * Last Modified: Monday, 1st February 2021 11:30:29 am
- * Modified By: Vicente Melin (vicente@inventures.cl)
+ * Last Modified: Monday, 17th May 2021 8:17:34 pm
+ * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
  * Copyright 2020 - 2020 Incrementa Ventures SpA. ALL RIGHTS RESERVED
  * Terms and conditions defined in license.txt
@@ -17,6 +17,8 @@ import { Formatter } from './Formatter';
 
 export class CurrencyFormatter extends Formatter {
   format(input: number) {
+    if (input === 0) return '$ 0';
+    if (!input) return '';
     return (
       '$ ' + String(input.toFixed(0)).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') //separate every 3 digits and use . to separate
     );
