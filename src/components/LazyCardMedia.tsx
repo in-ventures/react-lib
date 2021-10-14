ec2817714ff54d456d489226803b5f3023aa4960
Gabriel Ulloa <gabriel.ulloa.e@gmail.com>
2021-10-14 15:33:52 -0300

    fix: handle no IntersectionObserver

--- src/components/LazyCardMedia.tsx
+++ src/components/LazyCardMedia.tsx
@@ -4,7 +4,7 @@
  * File Created: Tuesday, 5th October 2021 6:27:22 pm
  * Author: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
- * Last Modified: Tuesday, 5th October 2021 7:19:50 pm
+ * Last Modified: Thursday, 14th October 2021 3:33:39 pm
  * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
  * -----
  * Copyright 2019 - 2021 Incrementa Ventures SpA. ALL RIGHTS RESERVED
@@ -32,6 +32,10 @@ export function LazyCardMedia({
   const [visible, setVisible] = useState<boolean>(false);
   const placeholderRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
+    if (typeof window.IntersectionObserver === 'undefined') {
+      setVisible(true);
+      return;
+    }
     if (!visible && placeholderRef.current) {
       const observer = new IntersectionObserver(([{ intersectionRatio }]) => {
         if (intersectionRatio > 0) {
