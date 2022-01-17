5f81c997120e08ca59ac474dc709f11be4d8a8ee
Luis Aparicio <luis@inventures.cl>
2022-01-17 10:42:14 -0300

    Fix: Adding navegation to slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -4,8 +4,8 @@
  * File Created: Wednesday, 28th April 2021 7:00:44 pm
  * Author: Esperanza Horn (esperanza@inventures.cl)
  * -----
- * Last Modified: Friday, 29th October 2021 2:10:30 pm
- * Modified By: Gabriel Ulloa (gabriel@inventures.cl)
+ * Last Modified: Monday, 17th January 2022 10:41:21 am
+ * Modified By: Luis Aparicio (luis@inventures.cl)
  * -----
  * Copyright 2020 - 2021 Incrementa Ventures SpA. ALL RIGHTS RESERVED
  * Terms and conditions defined in license.txt
@@ -14,16 +14,25 @@
  */
 
 import 'regenerator-runtime/runtime.js';
-import React from 'react';
-import { GridList, makeStyles, createStyles } from '@material-ui/core';
+import React, { useCallback, useMemo, useRef } from 'react';
+import {
+  GridList,
+  makeStyles,
+  createStyles,
+  IconButton,
+} from '@material-ui/core';
 import GridListTile from '@material-ui/core/GridListTile';
 import clsx from 'clsx';
+import { useIntersectionObserver } from '../hooks/useIntersection';
+import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
+import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded';
 
 type SliderProps = {
   items: React.ReactNode[];
   cellHeight?: number | 'auto' | undefined;
   cols?: number;
   spacing?: number;
+  navigationSpeed?: number;
   classes?: {
     root?: string;
     gridList?: string;
@@ -32,19 +41,61 @@ type SliderProps = {
   };
 };
 
+enum NavigationDirection {
+  Left = 'left',
+  Right = 'right',
+}
+
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
       display: 'flex',
       flexWrap: 'wrap',
       justifyContent: 'space-around',
+      position: 'relative',
     },
     gridList: {
       flexWrap: 'nowrap',
       width: '100%',
+      '& :last-child': {
+        marginRight: 2,
+      },
+      scrollBehavior: 'smooth',
+    },
+    noScrollBar: {
+      '&::-webkit-scrollbar': {
+        display: 'none',
+      },
+      '-ms-overflow-style': 'none' /* IE and Edge */,
+      'scrollbar-width': 'none' /* Firefox */,
     },
     customTiles: {
       height: '100%',
+      padding: 4,
+    },
+    navigationRight: {
+      height: 'inherit !important',
+      width: 'fit-content !important',
+      padding: '12px !important',
+      position: 'absolute',
+      right: 0,
+      zIndex: 1,
+      alignSelf: 'center',
+    },
+    navigationLeft: {
+      height: 'inherit !important',
+      width: 'fit-content !important',
+      padding: '12px !important',
+      position: 'absolute',
+      left: 0,
+      zIndex: 1,
+      alignSelf: 'center',
+    },
+    hidden: {
+      display: 'none',
+    },
+    tile: {
+      padding: 4,
     },
   }),
 );
@@ -58,19 +109,77 @@ export function Slider(props: SliderProps) {
    * @returns React Component
    *
    */
-  const { items, cellHeight, cols, classes: propsClasses, spacing = 8 } = props;
+  const {
+    items,
+    cellHeight,
+    cols,
+    classes: propsClasses,
+    spacing = 8,
+    navigationSpeed = 3,
+  } = props;
   const classes = useStyles();
+
+  const refFirst = useRef<HTMLLIElement | null>(null);
+  const refLast = useRef<HTMLLIElement | null>(null);
+  const sliderRef = useRef<HTMLUListElement | null>(null);
+
+  const leftNavigation = useIntersectionObserver(refFirst, { threshold: 1 });
+  const rightNavigation = useIntersectionObserver(refLast, { threshold: 1 });
+
+  const leftNavigationisVisible = useMemo(() => {
+    if (!leftNavigation) return;
+    return !!leftNavigation.isIntersecting;
+  }, [leftNavigation]);
+
+  const rightNavigationisVisible = useMemo(() => {
+    if (!rightNavigation) return;
+    return !!rightNavigation.isIntersecting;
+  }, [rightNavigation]);
+
+  const handleArrowClick = useCallback(
+    (direction: NavigationDirection) => {
+      const scrollElement = () => {
+        if (!sliderRef.current) return;
+        if (direction === NavigationDirection.Right) {
+          sliderRef.current.scrollLeft +=
+            sliderRef.current.clientWidth / navigationSpeed;
+        }
+        if (direction === NavigationDirection.Left) {
+          sliderRef.current.scrollLeft -=
+            sliderRef.current.clientWidth / navigationSpeed;
+        }
+      };
+      scrollElement();
+    },
+    [navigationSpeed],
+  );
 
   if (!items) return null;
   return (
     <div className={clsx(classes.root, propsClasses?.root)}>
+      <IconButton
+        onClick={() => handleArrowClick(NavigationDirection.Left)}
+        color="primary"
+        aria-label="move left"
+        className={clsx(
+          classes.navigationLeft,
+          leftNavigationisVisible && classes.hidden,
+        )}
+      >
+        <ArrowBackIosRoundedIcon />
+      </IconButton>
       <GridList
         className={clsx(classes.gridList, propsClasses?.gridList)}
         cols={cols ?? 1.5}
         cellHeight={cellHeight}
         classes={{
-          root: clsx(classes.customTiles, propsClasses?.gridListRoot),
+          root: clsx(
+            classes.customTiles,
+            classes.noScrollBar,
+            propsClasses?.gridListRoot,
+          ),
         }}
+        ref={sliderRef}
         spacing={spacing}
       >
         {items.map((item, index: number) => (
@@ -79,11 +188,25 @@ export function Slider(props: SliderProps) {
             classes={{
               root: clsx(classes.customTiles, propsClasses?.gridListTile),
             }}
+            ref={
+              index == 0 ? refFirst : index == items.length - 1 ? refLast : null
+            }
           >
             {item}
           </GridListTile>
         ))}
       </GridList>
+      <IconButton
+        onClick={() => handleArrowClick(NavigationDirection.Right)}
+        color="primary"
+        aria-label="move right"
+        className={clsx(
+          classes.navigationRight,
+          rightNavigationisVisible && classes.hidden,
+        )}
+      >
+        <ArrowForwardIosRoundedIcon />
+      </IconButton>
     </div>
   );
 }
